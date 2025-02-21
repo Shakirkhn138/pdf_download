36fbe96f35d80417aa385382e0ae81f06cc1791c
Shakirkhn138 <111078508+Shakirkhn138@users.noreply.github.com>
2025-02-21 16:55:50 +0500

    m

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pdf_download/View/home_screen/home_screen.dart';
+import 'package:pdf_download/home_screen.dart';
 
-import 'config/app_routers.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
