679db45a005435517f48a05993760d478d70db72
Shakirkhn138 <111078508+Shakirkhn138@users.noreply.github.com>
2025-02-24 16:42:57 +0500

    fonts changed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -75,14 +75,14 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.SizedBox(height: 10),
                   pw.Text(companyName, style: pw.TextStyle(
                       fontSize: 24, fontWeight: pw.FontWeight.bold)),
-                  pw.SizedBox(height: 70),
+                  pw.SizedBox(height: 80),
                   pw.Row(
                       children: [
                         pw.Text(pPayment, style: pw.TextStyle(
                             fontSize: 20, fontWeight: pw.FontWeight.bold)),
                         pw.SizedBox(width: 10),
                         pw.Text(pPaymentE, style: pw.TextStyle(
-                            fontSize: 18, color: PdfColors.grey)),
+                            fontSize: 18, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
 
                       ]
                   ),
@@ -93,18 +93,18 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.SizedBox(height: 10),
 
                   pw.Text(transferDateE, style: pw.TextStyle(
-                      fontSize: 18, color: PdfColors.grey)),
-                  pw.SizedBox(height: 40),
+                      fontSize: 18, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                  pw.SizedBox(height: 60),
 
                   pw.Row(
                       children: [
                         pw.Text(emitterName, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
                         pw.Text(emiterNameE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 60),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 170),
                         pw.Text(ets, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
                       ]
                   ),
                   pw.SizedBox(height: 20),
@@ -112,13 +112,13 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.Row(
                       children: [
                         pw.Text(emittersAccount, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
 
                         pw.Text(emittersAccountE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 10),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 95),
                         pw.Text(emittersAccountNumber, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
                       ]
                   ),
                   pw.SizedBox(height: 20),
@@ -126,13 +126,13 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.Row(
                       children: [
                         pw.Text(emittersBank, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
 
                         pw.Text(emiterNameE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 60),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 105),
                         pw.Text(emitterBankName, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
                       ]
                   ),
                   pw.SizedBox(height: 20),
@@ -140,13 +140,13 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.Row(
                       children: [
                         pw.Text(benefiery, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
 
                         pw.Text(beneficiaryE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 60),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 135),
                         pw.Text(beneficiaryName, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
                       ]
                   ),
                   pw.SizedBox(height: 20),
@@ -154,13 +154,13 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.Row(
                       children: [
                         pw.Text(benAccount, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
 
                         pw.Text(benAccountE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 10),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 70),
                         pw.Text(benAccountNumber, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
                       ]
                   ),
                   pw.SizedBox(height: 20),
@@ -168,13 +168,13 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.Row(
                       children: [
                         pw.Text(benBank, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
 
                         pw.Text(benBankE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 10),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 102),
                         pw.Text(benBankName, style: pw.TextStyle(
-                            fontSize: 18)),
+                            fontSize: 12)),
                       ]
                   ),
                   pw.SizedBox(height: 20),
@@ -182,26 +182,26 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   pw.Row(
                       children: [
                         pw.Text(montant, style: pw.TextStyle(
-                            fontSize: 18, fontWeight: pw.FontWeight.bold)),
+                            fontSize: 12, fontWeight: pw.FontWeight.bold)),
 
                         pw.Text(montantE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 60),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 205),
                         pw.Text(amount, style: pw.TextStyle(
-                            fontSize: 18,)),
+                            fontSize: 12,)),
                       ]
                   ),
                   pw.SizedBox(height: 20),
                   pw.Row(
                       children: [
                         pw.Text(reference, style: pw.TextStyle(
-                            fontSize: 18, fontWeight: pw.FontWeight.bold)),
+                            fontSize: 12, fontWeight: pw.FontWeight.bold)),
 
                         pw.Text(referenceE, style: pw.TextStyle(
-                            fontSize: 14, color: PdfColors.grey)),
-                        pw.SizedBox(width: 60),
+                            fontSize: 10, color: PdfColors.grey, fontStyle: pw.FontStyle.italic)),
+                        pw.SizedBox(width: 185),
                         pw.Text(referencer, style: pw.TextStyle(
-                            fontSize: 18,)),
+                            fontSize: 12,)),
                       ]
                   ),
                   pw.Spacer(),
@@ -246,7 +246,7 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
               ),
-              SizedBox(height: 50,),
+              SizedBox(height: 80,),
               Row(
                 children: [
                   Text(
@@ -258,39 +258,43 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                   ),
                   Text(
                     'PROOF OF PAYMENT',
-                    style: TextStyle(fontSize: 13, color: Colors.grey),
+                    style: TextStyle(fontSize: 13, color: Colors.grey, fontStyle: FontStyle.italic),
                   ),
                 ],
               ),
-              SizedBox(height: 10,),
-              Text(
-                'Le virement suivant a ete execute le 08/01/2025',
-                style: TextStyle(color: Colors.grey),
+              SizedBox(height: 40,),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  'Le virement suivant a ete execute le 08/01/2025',
+                  style: TextStyle(fontSize: 14),
+                ),
               ),
               Text(
                 'The following transfer has been completed on 08/01/2025',
-                style: TextStyle(color: Colors.grey),
+                style: TextStyle(color: Colors.grey, fontSize: 12, fontStyle: FontStyle.italic),
               ),
-              SizedBox(height: 10,),
+              SizedBox(height: 60,),
 
               Row(
                 children: [
                   Text(
                     'Emetteur',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
                     width: 8,
                   ),
                   Text(
                     '/Emitter\'s name',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
                   ),
-                  Spacer(),
+                  Spacer(
+                  ),
                   Text(
                     'ETS',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 12,
                     ),
                   ),
                 ],
@@ -301,19 +305,22 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                 children: [
                   Text(
                     'Compte de l\'emetteur',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   // SizedBox(width: 8,),
                   Text(
                     '/Emitter\'s account',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
                   ),
                 ],
               ),
-              Text(
-                'FR7616958000012389172080819',
-                style: TextStyle(
-                  fontSize: 16,
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  'FR7616958000012389172080819',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
                 ),
               ),
               SizedBox(height: 15,),
@@ -322,67 +329,74 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                 children: [
                   Text(
                     'Banque\'de l\'emetteur',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
                     width: 8,
                   ),
                   Text(
                     '/Emitter\'s bank',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
+                  ),
+                  Spacer(),
+                  Text(
+                    'QNTOFRP1XXX',
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
                   ),
                 ],
               ),
-              Text(
-                'QNTOFRP1XXX',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
+
               SizedBox(height: 15,),
 
               Row(
                 children: [
                   Text(
                     'Beneficieare',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
-                    width: 8,
+                    width: 3,
                   ),
                   Text(
                     '/Beneficiary\'s name',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
+                  ),
+                  Spacer(),
+                  Text(
+                    'Muhammad Najar Hossein',
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
                   ),
                 ],
               ),
-              Text(
-                'Muhammad Najar Hossein',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
+
               SizedBox(height: 15,),
 
               Row(
                 children: [
                   Text(
                     'Compte du beneficiare',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
                     width: 8,
                   ),
                   Text(
                     '/Beneficiary\'s account',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
                   ),
                 ],
               ),
-              Text(
-                'DE23370501981936451838',
-                style: TextStyle(
-                  fontSize: 16,
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  'DE23370501981936451838',
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
                 ),
               ),
               SizedBox(height: 15,),
@@ -391,43 +405,45 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                 children: [
                   Text(
                     'Banque du beneficiare',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
-                    width: 8,
+                    width: 5,
                   ),
                   Text(
                     '/Beneficiary\'s bank',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
+                  ),
+                  Spacer(),
+                  Text(
+                    'COLSDE33XXX',
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
                   ),
                 ],
               ),
-              Text(
-                'COLSDE33XXX',
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
+
               SizedBox(height: 15,),
 
               Row(
                 children: [
                   Text(
                     'Montant',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
                     width: 8,
                   ),
                   Text(
                     '/Amount',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
                   ),
                   Spacer(),
                   Text(
                     '10000.00 EUR',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 12,
                     ),
                   ),
                 ],
@@ -438,20 +454,20 @@ class _PDFDownloadScreenState extends State<PDFDownloadScreen> {
                 children: [
                   Text(
                     'Reference',
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(
                     width: 8,
                   ),
                   Text(
                     '/Reference',
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                    style: TextStyle(fontSize: 10, color: Colors.grey, fontStyle: FontStyle.italic),
                   ),
                   Spacer(),
                   Text(
                     'Facture',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 12,
                     ),
                   ),
                 ],
